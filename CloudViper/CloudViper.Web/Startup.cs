87ea330f129de60a841b3a939d97e1c3f489df11
Horacio Enrique Castillo Puente <horacio.castillopnt@bis.ute.edu.mx>
2018-11-08 00:28:29 -0600

    The context is typically configured in Startup.cs with the connection string being read from configuration. Note the GetConnectionString() method looks for a configuration value whose key is ConnectionStrings:<connection string name>. You need to import the Microsoft.Extensions.Configuration namespace to to use this extension method.

--- CloudViper/CloudViper.Web/Startup.cs
+++ CloudViper/CloudViper.Web/Startup.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CloudViper.Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -33,6 +35,10 @@ namespace CloudViper.Web
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            //var connection = @"server=localhost;user id=root;database=viper_developments;port=3306";
+            services.AddDbContext<ViperDbContext>
+                (options => options.UseMySql(Configuration.GetConnectionString("ViperDbContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

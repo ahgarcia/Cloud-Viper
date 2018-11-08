7b9c9db8be3fc642bc9d14d7e7e965372100c745
Horacio Enrique Castillo Puente <horacio.castillopnt@bis.ute.edu.mx>
2018-11-08 00:26:19 -0600

    Now, let us create the context class, which will be a middleware component for the communication with the database. It has DbSet properties which contain the table data from the database.
    To enable communication between the .NET core part and the MySQL database, you’ll need to install third-party library named Pomelo.EntityFrameworkCore.MySql. In the main project, you may install it with the NuGet package manager or Package manager console.

---
+++ CloudViper/CloudViper.Infrastructure/Data/ViperDbContext.cs
@@ -0,0 +1,58 @@
+﻿using CloudViper.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CloudViper.Infrastructure.Data
+{
+    public class ViperDbContext : DbContext
+    {
+        public ViperDbContext(DbContextOptions<ViperDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CountryRegion> CountriesRegion { get; set; }
+        public DbSet<StateProvince> StatesProvince { get; set; }
+        public DbSet<City> Cities { get; set; }
+        public DbSet<RoadType> RoadsType { get; set; }
+        public DbSet<AddressType> AddressesType { get; set; }
+        public DbSet<Gender> Genders { get; set; }
+        public DbSet<MaritalStatus> MaritalsStatus { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Shift> Shifts { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<JobTitle> JobsTitle { get; set; }
+        public DbSet<RegimenFiscal> RegimenesFiscales { get; set; }
+        public DbSet<Module> Modules { get; set; }
+        public DbSet<Permission> Permissions { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<AddressSAT> AddressesSAT { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Site> Sites { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<EmployeeDepartmentHistory> EmployeesDepartmentHistory { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<ProductCategory> ProductsCategory { get; set; }
+        public DbSet<ProductSubCategory> ProductsSubCategory { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductInventory> ProductsInventory { get; set; }
+        public DbSet<ProductCostHistory> ProductsCostHistory { get; set; }
+        public DbSet<ProductListPriceHistory> ProductsListPriceHistory { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            { 
+                optionsBuilder.UseMySql(@"server=localhost;user id=root;database=viper_developments;port=3306");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
